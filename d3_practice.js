d05680e15108719381ee349d7d5f954879d6319b
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-27 20:15:43 -0500

    created simple bar chart with no style

--- d3_practice.js
+++ d3_practice.js
@@ -1,4 +1,4 @@
-d3.select("body")
+/*d3.select("body")
 	.append("svg")
 		.attr("width",50)
 		.attr("height", 50)
@@ -16,4 +16,30 @@ d3.select("body")
 		.text("Savannah Ostrowski")
 		.attr("x", 0)
 		.attr("y", 25)
-		.style("fill", "purple")
+		.style("fill", "purple")*/
+
+var w = 500;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 15, 20, 25];
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr ("height", h);
+
+// i is the index/position of element, d is the dataset being passed in
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+		.attr("x", function(d, i){ 
+			return (i * (w / dataset.length));})
+		.attr("y", function(d){
+			return h - (d * 4);
+		})
+		.attr("width", w / dataset.length - padding)
+		.attr("height", function(d){
+			return (d * 4);
+		}
+			);
+	
