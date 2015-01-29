d2b02c818a7b7fd9846ce4f3cba91312c7303977
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-28 20:35:24 -0500

    created bar and line graph

--- d3_practice.js
+++ d3_practice.js
@@ -1,46 +1,46 @@
-// var w = 500;
-// var h = 100;
-// var padding = 2;
-// var dataset = [5, 10, 15, 20, 25, 5, 10, 15, 20, 25];
-// var svg = d3.select("body")
-// 			.append("svg")
-// 			.attr("width", w)
-// 			.attr ("height", h);
+var w = 500;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 15, 20, 25, 5, 10, 15, 20, 25];
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr ("height", h);
 
-// //color selector
-// function colourPicker(v){
-// 	if (v<=20) {return "#666666";}
-// 	else if (v>20) {return "#FF0033";}
-// }
+//color selector
+function colourPicker(v){
+	if (v<=20) {return "#666666";}
+	else if (v>20) {return "#FF0033";}
+}
 
 
-// // i is the index/position of element, d is the dataset being passed in
-// svg.selectAll("rect")
-// 	.data(dataset)
-// 	.enter()
-// 	.append("rect")
-// 		.attr({
-// 			x: function(d, i){return (i * (w / dataset.length));},
-// 		 	y: function(d){return h - (d * 4);},
-// 			width: w / dataset.length - padding,
-// 			height: function(d){return (d * 4);},
-// 			fill: function(d){return colourPicker(d);}
-// 		});
-// // labels for bar chart		
-// svg.selectAll("text")
-// 	.data(dataset)
-// 	.enter()
-// 	.append("text")
-// 	.text (function (d) { return d;})
-// 	.attr({
-// 		"text-anchor":"middle",
-// 		x: function(d, i){return i * (w / dataset.length) + 
-// 			(w / dataset.length - padding) / 2;},
-// 		y:function(d){return h - (d * 4) + 14;},
-// 		"font-family": "sans-serif",
-// 		"font-size": 12,
-// 		"fill": "#ffffff"
-// 	});
+// i is the index/position of element, d is the dataset being passed in
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+		.attr({
+			x: function(d, i){return (i * (w / dataset.length));},
+		 	y: function(d){return h - (d * 4);},
+			width: w / dataset.length - padding,
+			height: function(d){return (d * 4);},
+			fill: function(d){return colourPicker(d);}
+		});
+// labels for bar chart		
+svg.selectAll("text")
+	.data(dataset)
+	.enter()
+	.append("text")
+	.text (function (d) { return d;})
+	.attr({
+		"text-anchor":"middle",
+		x: function(d, i){return i * (w / dataset.length) + 
+			(w / dataset.length - padding) / 2;},
+		y:function(d){return h - (d * 4) + 14;},
+		"font-family": "sans-serif",
+		"font-size": 12,
+		"fill": "#ffffff"
+	});
 
 // line chart
 var h = 500;
