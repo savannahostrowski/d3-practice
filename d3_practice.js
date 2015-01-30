d121b3750d352d58c3b97c76ebfdd0613fa05ffa
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-01-29 20:07:33 -0500

    created scatterplot

--- d3_practice.js
+++ d3_practice.js
@@ -95,3 +95,74 @@ var labels = svg.selectAll("text")
 			return "normal";}
 		}
 		});
+
+//scatterplot
+var h = 350;
+var w = 400;
+monthlySales = [
+	{"month":10, "sales": 100},
+	{"month":20, "sales": 130},
+	{"month":30, "sales": 250},
+	{"month":40, "sales": 300},
+	{"month":50, "sales": 260},
+	{"month":60, "sales": 180},
+	{"month":70, "sales": 120},
+	{"month":80, "sales": 130}
+];
+
+//key perf indicator
+function salesKPI(d){
+	if (d >= 250){
+		return "#33CC66";
+	} else {
+		if (d < 250){
+			return "#666666";
+		}
+	}
+}
+
+function showMinMax(ds, col, val, type){
+	var max = d3.max(ds,  function(d){
+		return d[col];
+	});
+	var min = d3.min(ds,  function(d){
+		return d[col];
+	});
+	if (type == 'minmax' && (val == max || val == min)){
+		return val;
+	}else{
+		if (type == 'all'){
+			return val;
+		}
+	}
+}
+//create svg
+var svg = d3.select("body").append("svg").attr({width:w, height:h});
+
+//add dots
+var dots = svg.selectAll("circle")
+.data(monthlySales)
+.enter()
+.append("circle")
+.attr({
+	cx: function (d){ return d.month * 3;},
+	cy: function (d){ return h - d.sales;},
+	r: 5,
+	"fill": function (d){ return salesKPI(d.sales);}
+});
+
+//add labels
+var labels = svg.selectAll('text')
+.data(monthlySales)
+.enter()
+.append('text')
+//can switch 'minmax' to 'all' to display all labels
+.text(function (d) {return showMinMax(monthlySales, 'sales', d.sales, 'minmax');})
+.attr({
+x: function (d){return ((d.month * 3) - 25); },
+y: function(d){return h-d.sales; },
+	"font-size":"12px",
+	"font-family": "sans-serif",
+	"fill": "#666666",
+	"text-anchor": "start"
+});
